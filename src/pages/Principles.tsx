86984e1663e0263302216c2407e4354b4405df72
Eduardo Ferrari <eduferrarimagalhaes@hotmail.com>
2026-03-15 19:06:44 -0300

    [CHORE] Separa principios em arquivo externo

--- src/pages/Principles.tsx
+++ src/pages/Principles.tsx
@@ -1,10 +1,4 @@
-const principles = [
-  "Tecnologia é meio, não fim.",
-  "Clareza reduz retrabalho.",
-  "Velocidade importa, mas direção importa mais.",
-  "Sistemas bons escalam decisões, não só tarefas.",
-  "O simples bem feito costuma vencer o complexo mal mantido.",
-]
+import { principles } from "../data/principles"
 
 export default function Principles() {
   return (
